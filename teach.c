8aadbd0fe60bb9657de1478b3b6543f8f5e1e7b3
suoravi <ville.m.suoranta@gmail.com>
2025-02-01 00:15:11 +0200

    added the missing teach.c file

---
+++ teach.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Placeholder for cmd_teach_sv function
+void cmd_teach_sv(char *filename) {
+    // TODO: Implement teaching functionality
+    printf("Teaching session started for: %s\n", filename);
+}

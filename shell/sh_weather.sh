dfc97b98d18368e9c9fd4b7081719cdfcfb68777
suoravi <ville.m.suoranta@gmail.com>
2025-03-09 20:38:24 +0200

    graphical ascii forecast

--- shell/sh_weather.sh
+++ shell/sh_weather.sh
@@ -21,19 +21,7 @@ while true; do
     clear
     
     # Print a header with the current date and time.
-    echo "Weather Report - $(date)"
-    echo "-----------------------------------"
-	echo ""
-    
-    # Display the weather report using ansiweather.
-    ansiweather -l "Muurame,FI" -u metric -f 3 -a false -s true 
-	echo ""
-    ansiweather -l "Rovaniemi,FI" -u metric -f 3 -a false -s true 
-	ansiweather -l "Oulu,FI" -u metric -f 3 -a false -s true 
-    ansiweather -l "Lappeenranta,FI" -u metric -f 3 -a false -s true 
-  	ansiweather -l "Tampere,FI" -u metric -f 3 -a false -s true 
-	ansiweather -l "Helsinki,FI" -u metric -f 3 -a false -s true 
-	ansiweather -l "Turku,FI" -u metric -f 3 -a false -s true 
+	curl wttr.in/Muurame?m
 
     # Wait for one hour (3600 seconds) before the next update.
     sleep 3600

ae4b1baaa3004399b4717702bfa21e5e6c918fd9
suoravi <ville.m.suoranta@gmail.com>
2025-08-26 22:20:07 +0300

    home cleaning

--- users/ville/build_drawdemo.sh
+++
@@ -1,4 +0,0 @@
-gcc -O3 -march=native -ffast-math -fno-strict-aliasing -std=c11 -c libdraw.c -o libdraw.o
-gcc -O3 -march=native -ffast-math -fno-strict-aliasing -std=c11 drawdemo.c libdraw.o -lm -o drawdemo
-sudo ./drawdemo
-

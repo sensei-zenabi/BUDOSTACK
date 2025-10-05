341d939e576597d21f5a0f7f5da62cea531f5282
suoravi <ville.m.suoranta@gmail.com>
2025-10-05 21:32:21 +0300

    template for _EXE created

---
+++ commands/_EXE.c
@@ -0,0 +1,13 @@
+/*
+
+SYNTAX: _EXE -[x] -[y]
+
+DESCRIPTION:
+  
+  With _EXE command user is able to start executable with offset given by
+  [x] in columns and [y] as rows from top left corner. All output from
+  executables started with _EXE follow the background color without replacing
+  it. I.e. all characters printed will follow the character background cell
+  color instead of replacing it.
+
+*/

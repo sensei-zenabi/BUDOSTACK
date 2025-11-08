d8c0edeafc281ce02bffdb311b9ab227314f650f
suoravi <ville.m.suoranta@gmail.com>
2025-11-08 22:36:20 +0200

    improved help and extended capabilities to match math.h

--- commands/_CALC.c
+++ commands/_CALC.c
@@ -21,6 +21,152 @@ typedef struct {
     size_t pos;
 } Parser;
 
+typedef struct {
+    const char *name;
+    double (*func)(double);
+} UnaryFunction;
+
+typedef struct {
+    const char *name;
+    double (*func)(double, double);
+} BinaryFunction;
+
+typedef struct {
+    const char *name;
+    double (*func)(double, double, double);
+} TernaryFunction;
+
+typedef struct {
+    const char *name;
+    double value;
+} MathConstant;
+
+static double ldexp_wrapper(double value, double exponent);
+static double scalbn_wrapper(double value, double exponent);
+static double scalbln_wrapper(double value, double exponent);
+
+#define UNARY_FUNCTION_LIST                                                           \
+    X(abs, fabs)                                                                       \
+    X(acos, acos)                                                                      \
+    X(acosh, acosh)                                                                    \
+    X(asin, asin)                                                                      \
+    X(asinh, asinh)                                                                    \
+    X(atan, atan)                                                                      \
+    X(atanh, atanh)                                                                    \
+    X(cbrt, cbrt)                                                                      \
+    X(ceil, ceil)                                                                      \
+    X(cos, cos)                                                                        \
+    X(cosh, cosh)                                                                      \
+    X(erf, erf)                                                                        \
+    X(erfc, erfc)                                                                      \
+    X(exp, exp)                                                                        \
+    X(exp2, exp2)                                                                      \
+    X(expm1, expm1)                                                                    \
+    X(fabs, fabs)                                                                      \
+    X(floor, floor)                                                                    \
+    X(gamma, tgamma)                                                                   \
+    X(ln, log)                                                                         \
+    X(lgamma, lgamma)                                                                  \
+    X(log, log)                                                                        \
+    X(log10, log10)                                                                    \
+    X(log1p, log1p)                                                                    \
+    X(log2, log2)                                                                      \
+    X(tgamma, tgamma)                                                                  \
+    X(round, round)                                                                    \
+    X(sin, sin)                                                                        \
+    X(sinh, sinh)                                                                      \
+    X(sqrt, sqrt)                                                                      \
+    X(tan, tan)                                                                        \
+    X(tanh, tanh)                                                                      \
+    X(trunc, trunc)
+
+#define BINARY_FUNCTION_LIST                                                          \
+    X(atan2, atan2)                                                                   \
+    X(copysign, copysign)                                                             \
+    X(fdim, fdim)                                                                     \
+    X(fmax, fmax)                                                                     \
+    X(fmin, fmin)                                                                     \
+    X(fmod, fmod)                                                                     \
+    X(hypot, hypot)                                                                   \
+    X(pow, pow)                                                                       \
+    X(remainder, remainder)
+
+#define BINARY_WRAPPER_LIST                                                           \
+    X(ldexp, ldexp_wrapper)                                                           \
+    X(scalbn, scalbn_wrapper)                                                         \
+    X(scalbln, scalbln_wrapper)
+
+#define TERNARY_FUNCTION_LIST                                                         \
+    X(fma, fma)
+
+#define CONSTANT_LIST                                                                 \
+    X(e, BUDOSTACK_E)                                                                 \
+    X(inf, INFINITY)                                                                  \
+    X(infinity, INFINITY)                                                             \
+    X(nan, NAN)                                                                       \
+    X(pi, BUDOSTACK_PI)                                                               \
+    X(tau, 2.0 * BUDOSTACK_PI)
+
+static const UnaryFunction unary_functions[] = {
+#define X(name, func) {#name, func},
+    UNARY_FUNCTION_LIST
+#undef X
+};
+
+static const char *const unary_function_names[] = {
+#define X(name, func) #name,
+    UNARY_FUNCTION_LIST
+#undef X
+};
+
+static const BinaryFunction binary_functions[] = {
+#define X(name, func) {#name, func},
+    BINARY_FUNCTION_LIST
+#undef X
+};
+
+static const char *const binary_function_names[] = {
+#define X(name, func) #name,
+    BINARY_FUNCTION_LIST
+#undef X
+};
+
+static const BinaryFunction binary_wrappers[] = {
+#define X(name, func) {#name, func},
+    BINARY_WRAPPER_LIST
+#undef X
+};
+
+static const char *const binary_wrapper_names[] = {
+#define X(name, func) #name,
+    BINARY_WRAPPER_LIST
+#undef X
+};
+
+static const TernaryFunction ternary_functions[] = {
+#define X(name, func) {#name, func},
+    TERNARY_FUNCTION_LIST
+#undef X
+};
+
+static const char *const ternary_function_names[] = {
+#define X(name, func) #name,
+    TERNARY_FUNCTION_LIST
+#undef X
+};
+
+static const MathConstant constants[] = {
+#define X(name, value) {#name, value},
+    CONSTANT_LIST
+#undef X
+};
+
+static const char *const constant_names[] = {
+#define X(name, value) #name,
+    CONSTANT_LIST
+#undef X
+};
+
 static void skip_spaces(Parser *parser);
 static int peek_char(const Parser *parser);
 static int match_char(Parser *parser, char expected);
@@ -31,6 +177,7 @@ static double parse_unary(Parser *parser, int *error);
 static double parse_primary(Parser *parser, int *error);
 static double parse_function(Parser *parser, const char *name, int *error);
 static void print_help(void);
+static void print_wrapped_list(const char *title, const char *const *items, size_t count);
 
 static void skip_spaces(Parser *parser) {
     while (parser->input[parser->pos] != '\0' &&
@@ -88,11 +235,6 @@ static double parse_term(Parser *parser, int *error) {
             if (op == '*') {
                 value *= rhs;
             } else {
-                if (rhs == 0.0) {
-                    fprintf(stderr, "division by zero\n");
-                    *error = 1;
-                    return 0.0;
-                }
                 value /= rhs;
             }
         } else {
@@ -113,14 +255,7 @@ static double parse_power(Parser *parser, int *error) {
         if (*error != 0) {
             return 0.0;
         }
-        errno = 0;
-        double result = pow(base, exponent);
-        if (errno != 0 || isfinite(result) == 0) {
-            fprintf(stderr, "invalid exponentiation\n");
-            *error = 1;
-            return 0.0;
-        }
-        return result;
+        return pow(base, exponent);
     }
     return base;
 }
@@ -200,89 +335,91 @@ static double parse_primary(Parser *parser, int *error) {
 static double parse_function(Parser *parser, const char *name, int *error) {
     skip_spaces(parser);
     if (match_char(parser, '(') != 0) {
-        double argument = parse_expression(parser, error);
-        if (*error != 0) {
-            return 0.0;
-        }
-        skip_spaces(parser);
-        if (match_char(parser, ')') == 0) {
-            fprintf(stderr, "missing closing parenthesis for %s\n", name);
-            *error = 1;
-            return 0.0;
-        }
-        if (strcmp(name, "sqrt") == 0) {
-            if (argument < 0.0) {
-                fprintf(stderr, "sqrt domain error\n");
+        double *arguments = NULL;
+        size_t count = 0;
+        size_t capacity = 0;
+        while (1) {
+            skip_spaces(parser);
+            if (match_char(parser, ')') != 0) {
+                break;
+            }
+            double value = parse_expression(parser, error);
+            if (*error != 0) {
+                free(arguments);
+                return 0.0;
+            }
+            if (count == capacity) {
+                size_t new_capacity = capacity == 0 ? 4 : capacity * 2;
+                double *tmp = realloc(arguments, new_capacity * sizeof(*tmp));
+                if (tmp == NULL) {
+                    free(arguments);
+                    fprintf(stderr, "memory allocation failure\n");
+                    *error = 1;
+                    return 0.0;
+                }
+                arguments = tmp;
+                capacity = new_capacity;
+            }
+            arguments[count++] = value;
+            skip_spaces(parser);
+            if (match_char(parser, ')') != 0) {
+                break;
+            }
+            if (match_char(parser, ',') == 0) {
+                free(arguments);
+                fprintf(stderr, "expected ',' in argument list for %s\n", name);
                 *error = 1;
                 return 0.0;
             }
-            return sqrt(argument);
         }
-        if (strcmp(name, "sin") == 0) {
-            return sin(argument);
-        }
-        if (strcmp(name, "cos") == 0) {
-            return cos(argument);
-        }
-        if (strcmp(name, "tan") == 0) {
-            return tan(argument);
-        }
-        if (strcmp(name, "asin") == 0) {
-            if (argument < -1.0 || argument > 1.0) {
-                fprintf(stderr, "asin domain error\n");
-                *error = 1;
-                return 0.0;
+        double result = 0.0;
+        int handled = 0;
+        if (count == 1) {
+            for (size_t i = 0; i < sizeof(unary_functions) / sizeof(unary_functions[0]); ++i) {
+                if (strcmp(name, unary_functions[i].name) == 0) {
+                    result = unary_functions[i].func(arguments[0]);
+                    handled = 1;
+                    break;
+                }
             }
-            return asin(argument);
-        }
-        if (strcmp(name, "acos") == 0) {
-            if (argument < -1.0 || argument > 1.0) {
-                fprintf(stderr, "acos domain error\n");
-                *error = 1;
-                return 0.0;
+        } else if (count == 2) {
+            for (size_t i = 0; i < sizeof(binary_functions) / sizeof(binary_functions[0]); ++i) {
+                if (strcmp(name, binary_functions[i].name) == 0) {
+                    result = binary_functions[i].func(arguments[0], arguments[1]);
+                    handled = 1;
+                    break;
+                }
             }
-            return acos(argument);
-        }
-        if (strcmp(name, "atan") == 0) {
-            return atan(argument);
-        }
-        if (strcmp(name, "ln") == 0 || strcmp(name, "log") == 0) {
-            if (argument <= 0.0) {
-                fprintf(stderr, "log domain error\n");
-                *error = 1;
-                return 0.0;
+            if (handled == 0) {
+                for (size_t i = 0; i < sizeof(binary_wrappers) / sizeof(binary_wrappers[0]); ++i) {
+                    if (strcmp(name, binary_wrappers[i].name) == 0) {
+                        result = binary_wrappers[i].func(arguments[0], arguments[1]);
+                        handled = 1;
+                        break;
+                    }
+                }
             }
-            return log(argument);
-        }
-        if (strcmp(name, "log10") == 0) {
-            if (argument <= 0.0) {
-                fprintf(stderr, "log10 domain error\n");
-                *error = 1;
-                return 0.0;
+        } else if (count == 3) {
+            for (size_t i = 0; i < sizeof(ternary_functions) / sizeof(ternary_functions[0]); ++i) {
+                if (strcmp(name, ternary_functions[i].name) == 0) {
+                    result = ternary_functions[i].func(arguments[0], arguments[1], arguments[2]);
+                    handled = 1;
+                    break;
+                }
             }
-            return log10(argument);
         }
-        if (strcmp(name, "exp") == 0) {
-            return exp(argument);
+        free(arguments);
+        if (handled != 0) {
+            return result;
         }
-        if (strcmp(name, "abs") == 0) {
-            return fabs(argument);
-        }
-        if (strcmp(name, "ceil") == 0) {
-            return ceil(argument);
-        }
-        if (strcmp(name, "floor") == 0) {
-            return floor(argument);
-        }
-        fprintf(stderr, "unknown function '%s'\n", name);
+        fprintf(stderr, "unknown function '%s' with %zu argument(s)\n", name, count);
         *error = 1;
         return 0.0;
     }
-    if (strcmp(name, "pi") == 0) {
-        return BUDOSTACK_PI;
-    }
-    if (strcmp(name, "e") == 0) {
-        return BUDOSTACK_E;
+    for (size_t i = 0; i < sizeof(constants) / sizeof(constants[0]); ++i) {
+        if (strcmp(name, constants[i].name) == 0) {
+            return constants[i].value;
+        }
     }
     fprintf(stderr, "unknown identifier '%s'\n", name);
     *error = 1;
@@ -292,10 +429,65 @@ static double parse_function(Parser *parser, const char *name, int *error) {
 static void print_help(void) {
     puts("BUDOSTACK Calculator");
     puts("Usage: _CALC <expression>");
-    puts("Supports: +, -, *, /, ^, parentheses, unary +/-");
-    puts("Functions: sqrt, sin, cos, tan, asin, acos, atan, ln/log, log10, exp, abs, ceil, floor");
-    puts("Constants: pi, e");
+    puts("");
+    puts("Operators:");
+    puts("    +, -, *, /, ^, parentheses, unary +/-");
+    puts("");
+    print_wrapped_list("Unary functions", unary_function_names,
+                       sizeof(unary_function_names) / sizeof(unary_function_names[0]));
+    print_wrapped_list("Binary functions", binary_function_names,
+                       sizeof(binary_function_names) / sizeof(binary_function_names[0]));
+    print_wrapped_list("Binary functions (integer exponent helpers)", binary_wrapper_names,
+                       sizeof(binary_wrapper_names) / sizeof(binary_wrapper_names[0]));
+    print_wrapped_list("Ternary functions", ternary_function_names,
+                       sizeof(ternary_function_names) / sizeof(ternary_function_names[0]));
+    print_wrapped_list("Constants", constant_names,
+                       sizeof(constant_names) / sizeof(constant_names[0]));
+    puts("");
     puts("Example: _CALC (1*2 + 3) / 2 + 2^2 + sqrt(5) + sin(pi)");
+}
+
+static void print_wrapped_list(const char *title, const char *const *items, size_t count) {
+    if (count == 0) {
+        return;
+    }
+    printf("%s:\n", title);
+    const size_t wrap_width = 80;
+    size_t line_length = 0;
+    for (size_t i = 0; i < count; ++i) {
+        const char *name = items[i];
+        size_t name_length = strlen(name);
+        size_t separator_length = (line_length == 0) ? 4 : 2;
+        size_t projected_length = (line_length == 0) ? separator_length + name_length
+                                                     : line_length + separator_length + name_length;
+        if (line_length != 0 && projected_length > wrap_width) {
+            putchar('\n');
+            line_length = 0;
+            separator_length = 4;
+            projected_length = separator_length + name_length;
+        }
+        if (line_length == 0) {
+            printf("    %s", name);
+            line_length = separator_length + name_length;
+        } else {
+            printf(", %s", name);
+            line_length = projected_length;
+        }
+    }
+    putchar('\n');
+    putchar('\n');
+}
+
+static double ldexp_wrapper(double value, double exponent) {
+    return ldexp(value, (int)exponent);
+}
+
+static double scalbn_wrapper(double value, double exponent) {
+    return scalbn(value, (int)exponent);
+}
+
+static double scalbln_wrapper(double value, double exponent) {
+    return scalbln(value, (long)exponent);
 }
 
 int main(int argc, char **argv) {

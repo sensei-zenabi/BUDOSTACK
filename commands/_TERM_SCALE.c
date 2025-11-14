2393bb3a075d17c1ac58bdc20a65db0bc83c8224
sensei-zenabi <ville.m.suoranta@gmail.com>
2025-11-14 21:02:20 +0200

    Add terminal scaling command and dynamic resize support

---
+++ commands/_TERM_SCALE.c
@@ -0,0 +1,43 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_usage(void) {
+    fprintf(stderr, "Usage: _TERM_SCALE <scale>\n");
+    fprintf(stderr, "  scale must be an integer between 1 and 4.\n");
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        print_usage();
+        return EXIT_FAILURE;
+    }
+
+    char *endptr = NULL;
+    errno = 0;
+    long value = strtol(argv[1], &endptr, 10);
+    if (errno != 0 || !endptr || *endptr != '\0') {
+        fprintf(stderr, "_TERM_SCALE: invalid scale value '%s'\n", argv[1]);
+        print_usage();
+        return EXIT_FAILURE;
+    }
+
+    if (value < 1 || value > 4) {
+        fprintf(stderr, "_TERM_SCALE: scale must be between 1 and 4\n");
+        return EXIT_FAILURE;
+    }
+
+    if (printf("\x1b]777;scale=%ld\a", value) < 0) {
+        perror("_TERM_SCALE: printf");
+        return EXIT_FAILURE;
+    }
+
+    if (fflush(stdout) != 0) {
+        perror("_TERM_SCALE: fflush");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}

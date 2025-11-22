4d0021a9b455c349d1d7862cbcb5772e7bbae9b5
suoravi <ville.m.suoranta@gmail.com>
2025-11-23 00:26:47 +0200

    renamed the shaders

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 # This shell script starts BUDOSTACK directly to it's in-built terminal application
-./apps/terminal -s ./shaders/film_noise.glsl -s ./shaders/vhs.glsl -s ./shaders/fakelottes-geom.glsl
+
+./apps/terminal -s ./shaders/noise.glsl -s ./shaders/effects.glsl -s ./shaders/crtscreen.glsl
+
 #./apps/terminal

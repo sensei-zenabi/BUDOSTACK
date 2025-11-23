00a82a5bf4cdb9c81a4d78f1a0797ab717c6b550
sensei-zenabi <ville.m.suoranta@gmail.com>
2025-11-23 22:39:39 +0200

    Handle render flag in _TERM_RENDER

--- commands/_TERM_RENDER.c
+++ commands/_TERM_RENDER.c
@@ -5,22 +5,32 @@
 #include <string.h>
 
 static void print_usage(void) {
-    fprintf(stderr, "Usage: _TERM_RENDER\\n");
-    fprintf(stderr, "  Triggers rendering of pending terminal pixel buffer.\\n");
+    fprintf(stderr, "Usage: _TERM_RENDER [--render]\n");
+    fprintf(stderr, "  Triggers rendering of pending terminal pixel buffer.\n");
 }
 
 int main(int argc, char **argv) {
-    if (argc > 1) {
-        if (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
+    int render = 0;
+
+    if (argc == 1) {
+        render = 1;
+    } else if (argc == 2) {
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
             print_usage();
             return EXIT_SUCCESS;
         }
 
+        if (strcmp(argv[1], "--render") == 0) {
+            render = 1;
+        }
+    }
+
+    if (!render) {
         print_usage();
         return EXIT_FAILURE;
     }
 
-    if (printf("\\x1b]777;pixel=render\\a") < 0) {
+    if (printf("\x1b]777;pixel=render\a") < 0) {
         perror("_TERM_RENDER: printf");
         return EXIT_FAILURE;
     }

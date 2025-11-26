75c39f59fcd9ad0cdbdc9fadd875b0582d2a14d6
suoravi <ville.m.suoranta@gmail.com>
2025-11-26 21:29:24 +0200

    aligned with array support

--- utilities/help.c
+++ utilities/help.c
@@ -252,9 +252,9 @@ int main(int argc, char *argv[]) {
     printf("\n");
     printf("  audiodemo.task       : Collection of in-built sound capabilities\n");
     printf("  autoexec.task        : Determines what happens before BUDOSTACK login screen\n");
+    printf("  codedemo.task        : Coding basics, for, while, arrays, etc.\n");
     printf("  csvdemo.task         : Dynamic .csv file manipulation demo\n");
-    printf("  demo.task            : Work-In-Progress\n");
-    printf("  fcntest.task         : Demonstrates function use\n");
+    printf("  demo.task            : Work-In-Progress\n");    
     printf("  release.task         : Used to generate release notes\n");    
     printf("  screen.task          : Screen calibration TASK\n");
     printf("  unicode.task         : Unicode character support unit-test\n");

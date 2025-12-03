c622537e188f13db14664157d3f0e8e7c20c6ab2
sensei-zenabi <ville.m.suoranta@gmail.com>
2025-12-03 22:52:38 +0200

    Ensure _TERM_MOUSE talks to controlling terminal

--- commands/_TERM_MOUSE.c
+++ commands/_TERM_MOUSE.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,20 +17,31 @@ static void print_usage(void) {
     printf("number of button presses since the last invocation.\n");
 }
 
-static int send_mouse_query(void) {
-    if (printf("\x1b]777;mouse=query\a") < 0) {
-        perror("_TERM_MOUSE: printf");
+static int send_mouse_query(int fd) {
+    if (fd < 0) {
         return -1;
     }
-    if (fflush(stdout) != 0) {
-        perror("_TERM_MOUSE: fflush");
-        return -1;
+
+    const char query[] = "\x1b]777;mouse=query\a";
+    size_t total_written = 0u;
+
+    while (total_written < sizeof(query) - 1u) {
+        ssize_t w = write(fd, query + total_written, (size_t)(sizeof(query) - 1u - total_written));
+        if (w < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            perror("_TERM_MOUSE: write");
+            return -1;
+        }
+        total_written += (size_t)w;
     }
+
     return 0;
 }
 
-static int read_mouse_response(int *out_x, int *out_y, unsigned int *out_left, unsigned int *out_right) {
-    if (!out_x || !out_y || !out_left || !out_right) {
+static int read_mouse_response(int fd, int *out_x, int *out_y, unsigned int *out_left, unsigned int *out_right) {
+    if (fd < 0 || !out_x || !out_y || !out_left || !out_right) {
         return -1;
     }
 
@@ -39,13 +51,13 @@ static int read_mouse_response(int *out_x, int *out_y, unsigned int *out_left, u
     for (;;) {
         fd_set rfds;
         FD_ZERO(&rfds);
-        FD_SET(STDIN_FILENO, &rfds);
+        FD_SET(fd, &rfds);
 
         struct timeval tv;
         tv.tv_sec = 1;
         tv.tv_usec = 0;
 
-        int ready = select(STDIN_FILENO + 1, &rfds, NULL, NULL, &tv);
+        int ready = select(fd + 1, &rfds, NULL, NULL, &tv);
         if (ready < 0) {
             if (errno == EINTR) {
                 continue;
@@ -58,7 +70,7 @@ static int read_mouse_response(int *out_x, int *out_y, unsigned int *out_left, u
             return -1;
         }
 
-        ssize_t rd = read(STDIN_FILENO, buffer + offset, sizeof(buffer) - offset - 1u);
+        ssize_t rd = read(fd, buffer + offset, sizeof(buffer) - offset - 1u);
         if (rd < 0) {
             if (errno == EINTR) {
                 continue;
@@ -115,7 +127,16 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    if (send_mouse_query() != 0) {
+    int tty_fd = open("/dev/tty", O_RDWR);
+    if (tty_fd < 0) {
+        fprintf(stderr, "_TERM_MOUSE: failed to open /dev/tty, falling back to stdout/stdin\n");
+        tty_fd = STDOUT_FILENO;
+    }
+
+    if (send_mouse_query(tty_fd) != 0) {
+        if (tty_fd != STDOUT_FILENO) {
+            close(tty_fd);
+        }
         return EXIT_FAILURE;
     }
 
@@ -123,8 +144,20 @@ int main(int argc, char **argv) {
     int y = 0;
     unsigned int left = 0u;
     unsigned int right = 0u;
-    if (read_mouse_response(&x, &y, &left, &right) != 0) {
+    int read_fd = tty_fd;
+    if (tty_fd == STDOUT_FILENO) {
+        read_fd = STDIN_FILENO;
+    }
+
+    if (read_mouse_response(read_fd, &x, &y, &left, &right) != 0) {
+        if (tty_fd != STDOUT_FILENO) {
+            close(tty_fd);
+        }
         return EXIT_FAILURE;
+    }
+
+    if (tty_fd != STDOUT_FILENO) {
+        close(tty_fd);
     }
 
     printf("{%d, %d, %u, %u}\n", x, y, left, right);

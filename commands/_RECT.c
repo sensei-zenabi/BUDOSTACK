40ef7597ae7c3681871965272e468a9dd5eda643
sensei-zenabi <ville.m.suoranta@gmail.com>
2025-12-10 18:40:32 +0200

    Improve _RECT fill option parsing

--- commands/_RECT.c
+++ commands/_RECT.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "../lib/retroprofile.h"
 #include "../lib/termbg.h"
@@ -62,6 +63,24 @@ static int parse_int(const char *value, const char *name, int *out) {
     return 0;
 }
 
+static int parse_fill(const char *value, int *fill_out) {
+    if (value == NULL || fill_out == NULL)
+        return -1;
+
+    if (strcasecmp(value, "on") == 0 || strcasecmp(value, "true") == 0 || strcmp(value, "1") == 0) {
+        *fill_out = 1;
+        return 0;
+    }
+
+    if (strcasecmp(value, "off") == 0 || strcasecmp(value, "false") == 0 || strcmp(value, "0") == 0) {
+        *fill_out = 0;
+        return 0;
+    }
+
+    fprintf(stderr, "_RECT: invalid value for -fill (expected on/off, true/false, or 1/0): '%s'\n", value);
+    return -1;
+}
+
 int main(int argc, char *argv[]) {
     int x = -1;
     int y = -1;
@@ -111,14 +130,8 @@ int main(int argc, char *argv[]) {
                 fprintf(stderr, "_RECT: missing value for -fill\n");
                 return EXIT_FAILURE;
             }
-            if (strcmp(argv[i], "on") == 0) {
-                fill = 1;
-            } else if (strcmp(argv[i], "off") == 0) {
-                fill = 0;
-            } else {
-                fprintf(stderr, "_RECT: invalid value for -fill (expected 'on' or 'off'): '%s'\n", argv[i]);
+            if (parse_fill(argv[i], &fill) != 0)
                 return EXIT_FAILURE;
-            }
         } else {
             fprintf(stderr, "_RECT: unknown argument '%s'\n", argv[i]);
             return EXIT_FAILURE;

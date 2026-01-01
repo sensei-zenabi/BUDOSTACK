5d9a42a4d84f3d8fcbbdccf5d6bbd2cfc374f6e6
suoravi <ville.m.suoranta@gmail.com>
2026-01-01 21:27:46 +0200

    update

--- utilities/gitter.c
+++ utilities/gitter.c
@@ -10,19 +10,23 @@
  *   a reserved section is printed for future hidden features or advanced help.
  */
 int main() {
-    // Print header information
-    printf("\n");
-    printf("================== GITTER - I remember so you don't have to!  ================\n");
-    printf("\n");
-    printf("  git log --name-only         : display commits and related files\n\n");
-    printf("  git commit -m 'message'     : make a commit with message\n\n");
-    printf("  git commit -m 'closes #nn'  : close issue with commit message\n\n");
-    printf("  git fetch origin            : fetch available branches\n\n");
-    printf("  git checkout <branch_name>  : switch into a selected branch\n\n");
-    printf("  git merge <branch_name>     : merge selected branch into current branch\n\n");
-    printf("  git tag --sort=-v:refname   : list tags from newest to oldest\n\n");
-    printf("  git revert <commit_hash>    : reverts a bad commit, do 'git push' afterwards\n\n");
-    printf("  git ls-files --others       : display non tracked files\n\n");
+  // Print header information
+  printf("\n"
+         "================== GITTER - I remember so you don't have to!  ================\n"
+         "\n"
+         "  git log --name-only         : display commits and related files\n\n"
+         "  git commit -m 'message'     : make a commit with message\n\n"
+         "  git commit -m 'closes #nn'  : close issue with commit message\n\n"
+         "  git fetch origin            : fetch available branches\n\n"
+         "  git checkout <branch_name>  : switch into a selected branch\n\n"
+         "  git merge <branch_name>     : merge selected branch into current branch\n\n"
+         "  git tag --sort=-v:refname   : list tags from newest to oldest\n\n"
+         "  git revert <commit_hash>    : reverts a bad commit, do 'git push' afterwards\n\n"
+         "  git ls-files --others       : display non tracked files\n\n"
+         "                              : force branch as new main HEAD\n"
+         "  git checkout main\n"
+         "  git reset --hard feature/new-architecture\n"
+         "  git push --force origin main\n\n");
                 
-    return 0;
+  return 0;
 }

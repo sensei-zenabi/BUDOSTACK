b0d8d7656507faa0cddb0b07b87397a06bdaf813
suoravi <ville.m.suoranta@gmail.com>
2026-01-05 23:20:08 +0200

    update

--- games/example.c
+++ games/example.c
@@ -7,14 +7,18 @@
 int main(void) {
 
   // Set resolution
-  budo_graphics_set_resolution(BUDO_SCREEN_WIDTH, BUDO_SCREEN_HEIGHT);
+  budo_graphics_set_resolution(640, 360);
 
   // Let's measure the drawing speed in comparison to TASK scripts
-  for (int x=1; x<10; x++) {
-    for (int y=1; y<10; y++) {
-      budo_graphics_draw_pixel(x, y, 255, 200, 100, 0);
+  for (int x=1; x<50; x++) {
+    for (int y=1; y<50; y++) {
+      budo_graphics_draw_pixel(x, y, 255, 200, 100, 1);
     }
   }
+  
+  budo_graphics_render_layer(1);
+
+  budo_graphics_clear_screen(640, 360, 1);
 
   return 0;
 

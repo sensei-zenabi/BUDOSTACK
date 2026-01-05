4727934c3d7617da9007bbdd0d8d18ca177ea0b3
suoravi <ville.m.suoranta@gmail.com>
2026-01-05 23:44:53 +0200

    benchmark

--- games/example.c
+++ games/example.c
@@ -1,24 +1,24 @@
 #include "../budo/budo_graphics.h"
 #include "../budo/budo_input.h"
 
-#define BUDO_SCREEN_WIDTH 640
-#define BUDO_SCREEN_HEIGHT 360
+#define WIDTH 320
+#define HEIGHT 200
 
 int main(void) {
 
   // Set resolution
-  budo_graphics_set_resolution(640, 360);
+  budo_graphics_set_resolution(WIDTH, HEIGHT);
 
   // Let's measure the drawing speed in comparison to TASK scripts
-  for (int x=1; x<50; x++) {
-    for (int y=1; y<50; y++) {
+  for (int x=1; x<WIDTH; x++) {
+    for (int y=1; y<HEIGHT; y++) {
       budo_graphics_draw_pixel(x, y, 255, 200, 100, 1);
     }
   }
   
   budo_graphics_render_layer(1);
 
-  budo_graphics_clear_screen(640, 360, 1);
+  //budo_graphics_clear_screen(640, 360, 1);
 
   return 0;
 

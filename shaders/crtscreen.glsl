a8c161adc56271a4c2aed870d1910a9acecf3f37
suoravi <ville.m.suoranta@gmail.com>
2026-01-07 21:53:48 +0200

    adjusted monitor gamma

--- shaders/crtscreen.glsl
+++ shaders/crtscreen.glsl
@@ -30,7 +30,7 @@
 #pragma parameter maskDark "maskDark" 0.5 0.0 2.0 0.1
 #pragma parameter maskLight "maskLight" 1.5 0.0 2.0 0.1
 #pragma parameter crt_gamma "CRT Gamma" 2.4 1.0 4.0 0.05
-#pragma parameter monitor_gamma "Monitor Gamma" 2.2 1.0 4.0 0.05
+#pragma parameter monitor_gamma "Monitor Gamma" 2.3 1.0 4.0 0.05
 #pragma parameter SCANLINE_SINE_COMP_A "Scanline Sine Comp A" 0.05 0.0 0.10 0.01
 #pragma parameter SCANLINE_SINE_COMP_B "Scanline Intensity" 0.5 0.0 1.0 0.05
 #pragma parameter SCANLINE_BASE_BRIGHTNESS "Scanline Base Brightness" 0.95 0.0 3.0 0.01

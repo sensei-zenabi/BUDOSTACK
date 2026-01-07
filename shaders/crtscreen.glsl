606e72180e661da140d0a975767fc178ff45b3d1
suoravi <ville.m.suoranta@gmail.com>
2026-01-07 23:14:21 +0200

    last improvement

--- shaders/crtscreen.glsl
+++ shaders/crtscreen.glsl
@@ -27,7 +27,7 @@
 #pragma parameter shadowMask "shadowMask" 2.0 0.0 4.0 1.0
 #pragma parameter warpX "warpX" 0.02 0.0 0.125 0.01
 #pragma parameter warpY "warpY" 0.02 0.0 0.125 0.01
-#pragma parameter maskDark "maskDark" 0.8 0.0 2.0 0.1
+#pragma parameter maskDark "maskDark" 0.6 0.0 2.0 0.1
 #pragma parameter maskLight "maskLight" 1.8 0.0 2.0 0.1
 #pragma parameter crt_gamma "CRT Gamma" 2.5 1.0 4.0 0.05
 #pragma parameter monitor_gamma "Monitor Gamma" 2.5 1.0 4.0 0.05

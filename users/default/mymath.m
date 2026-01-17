a432821a415952ee9550aa894e177651ab5b97ca
villen pöytäkone <ville.m.suoranta@gmail.com>
2026-01-18 00:00:12 +0200

    updated

--- users/default/mymath.m
+++ users/default/mymath.m
@@ -1,5 +1,34 @@
+#====================================================================
+# mymath.m
+#
+# Description:
+# 
+#  This cmath script demonstrates the capabilites of cmath powered 
+#  by _CALC math engine.
+#
+#  Run the script like this:
+#
+#    cmath mymath.m 10 2
+#    5, 50 
+#
+#  i.e. cmath is able to take your input values, run the script and
+#  return the results back to commandline in stdout.
+#
+#====================================================================
+
 print "This is example math application:"
-u1 = 12
-r1 = 120
-print "calculating [i=u/r]:"
-i1 = u1/r1
+
+U = $input1
+R = $input2
+
+print "calculating current and power..."
+
+I = U / R
+P = U * I
+
+print "outputting both as array"
+
+OUT = [I,P]
+
+return OUT
+

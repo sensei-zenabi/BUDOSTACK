bdbe09c15f96441879dcd797047c819889c0e974
villen pöytäkone <ville.m.suoranta@gmail.com>
2026-01-31 23:54:02 +0200

    working three pass

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 # This shell script starts BUDOSTACK directly to it's in-built terminal application
 
 ./apps/terminal \
+    -s ./shaders/crtscreen.glsl \
     -s ./shaders/noise.glsl \
-    -s ./shaders/crtscreen.glsl
+    -s ./shaders/effects.glsl
 
 #./apps/terminal

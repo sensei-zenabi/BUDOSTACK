a55c52c74e4798b946c7d75db73b0b724f185e7d
suoravi <ville.m.suoranta@gmail.com>
2026-02-01 18:33:28 +0200

    three pass with help

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
+#===================================================================================
 # This shell script starts BUDOSTACK directly to it's in-built terminal application
+#
+# Description:
+#
+#  noise.glsl     = Simulates stochastic noise of CRT screen.
+#  effects.glsl   = Simulates phosphor decay and chroma bleed.
+#  crtscreen.glsl = Simulates the CRT display curvature and phosphor mask.
+#===================================================================================
 
 ./apps/terminal \
     -s ./shaders/noise.glsl \
-    -s ./shaders/crtscreen.glsl \
-    -s ./shaders/effects.glsl
+    -s ./shaders/effects.glsl \
+    -s ./shaders/crtscreen.glsl
 
-#./apps/terminal
